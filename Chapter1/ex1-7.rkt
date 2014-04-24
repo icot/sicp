a59b18047d4930d39b5a02278f93112da80e953c
icot <ignacio.coterillo@gmail.com>
2014-04-24 21:24:27 +0200

    Ex 1.7 skeleton

---
+++ Chapter1/ex1-7.rkt
@@ -0,0 +1,11 @@
+#lang racket
+(require racket/include)
+
+(include "sqrt.rkt")
+
+; Guess: Given the applicative order evaluation of the interpreter,
+; evaluating the new-if body will allways run the sqrt-iter from the else 
+; clause 
+
+(module* main #f
+    (sqrt 2))
