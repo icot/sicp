edee45f3b751e9658954b50f959466822660890e
icot <icot@users.noreply.github.com>
2014-04-26 16:08:22 +0200

    Exercise 1-10. Ackermann Function

---
+++ Chapter1/ex1-10.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+; Ackermann's function
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+;(define (f n) (A 0 n)) -> f(n) = 2*n
+;(define (g n) (A 1 n)) -> g(n) = 2^n
+;(define (h n) (A 2 n)) -> h(n) = 2^(h(n-1)) 
+
+(module+ main
+    (/ (log (A 2 5)) (log 2)))
