1a7da26669b59dfd7ce7209da639769b1294846d
icot <icot@users.noreply.github.com>
2014-04-26 15:40:08 +0200

    Added answer in comments. Changed require

--- Chapter1/ex1-7.rkt
+++ Chapter1/ex1-7.rkt
@@ -1,7 +1,13 @@
 #lang racket
-(require racket/include)
+(require "sqrt.rkt")
 
-(include "sqrt.rkt")
+; For very small numbers (x << 1) good-enough? will return true in
+; 4 iterations when the initial guess (1.0) has been reduced by the 
+; averaging to ~1/16 and will cause the condition check in good-enough? 
+; to return true.
+
+; For very big numbers, the method seems to work ok. May be a difference
+; of implementation with regard to the original interpreter?
 
 (module+ main
-    (sqrt 2))
+    (sqrt1 1e20))
