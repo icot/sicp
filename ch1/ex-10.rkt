35931f03011dcad3515253da86aef390ad50f80c
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2020-05-02 09:33:41 +0200

    Add comments on ex-10

--- ch1/ex-10.rkt
+++ ch1/ex-10.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
-; Ackermann's function
+;; Ackermann's function
+;; https://en.wikipedia.org/wiki/Ackermann_function
+
 (define (A x y)
   (cond ((= y 0) 0)
         ((= x 0) (* 2 y))
@@ -8,9 +10,18 @@
         (else (A (- x 1)
                  (A x (- y 1))))))
 
-;(define (f n) (A 0 n)) -> f(n) = 2*n
-;(define (g n) (A 1 n)) -> g(n) = 2^n
-;(define (h n) (A 2 n)) -> h(n) = 2^(h(n-1)) 
+;; a) (define (f n) (A 0 n)) -> f(n) = 2*n
+
+;; b) (define (g n) (A 1 n)) -> g(n) = 2^n
+;; g(n) = A(0, A(1, n-1)) = 2 * A(1, n-1) = 2 * 2 * A(1, n-2)  = ... = 2^n
+
+;; d) (define (h n) (A 2 n)) -> h(n) = 2^(g(n-1)) = 2^(2^(n-1))
+
+;; h(n) = A(2, n) = A(1, A(2, n-1)) = g(A(2, n-1)) = 2^(A(2, n-1))
+;; recursively: 2^(2^(2^(2... = 2 (arrow) n
+;; where the Arrow comes from Knuth's arrow notation
+;; https://en.wikipedia.org/wiki/Knuth%27s_up-arrow_notation
+
 
 (module+ main
     (/ (log (A 2 5)) (log 2)))
