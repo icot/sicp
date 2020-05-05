6245a80fa91e3494f1a0e241cd756ec412eb98f0
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2020-05-05 19:38:34 +0200

    ch1/ex-22

--- ch1/ex-22.rkt
+++ ch1/ex-22.rkt
@@ -2,19 +2,40 @@
 
 (require "primes.rkt")
 
-(define (next-three-primes n acc)
-  (printf "n ~a acc ~a\n" n acc)
-  (if (< acc 3)
-      (if (prime? n)
-          ((printf "n ~a\n" n)
-           (next-three-primes (+ n 2) (+ acc 1)))
-          (next-three-primes (+ n 2) acc)
-          )
-      acc
+;; using (current-inexact-milliseconds) instead of runtime
+(define runtime current-inexact-milliseconds)
+
+(define (timed-prime-test n)
+  (printf "N: ~a\n" n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))
+      #t
       )
   )
 
 
-(module+ main
-  (time (next-three-primes 10001 0))
+(define (report-prime elapsed-time)
+  (printf " *** \n")
+  (printf "~a\n" elapsed-time)
+  #f
   )
+
+(define (search-for-primes n acc)
+  ;;(print "ACC: ~a" acc)
+  (if (< acc 3)
+      (if (timed-prime-test n)
+          (search-for-primes (+ n 2) acc)
+          (search-for-primes (+ n 2) (+ acc 1))
+          )
+      #t
+      )
+  )
+
+(module+ main
+ (search-for-primes 1001 0)
+ (search-for-primes 10001 0)
+ (search-for-primes 100001 0)
+ (search-for-primes 1000001 0))
